d06e0560ec7f26c1e9df71a2f9d702f571853628
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2021-07-02 18:49:04 +0200

    added docs

--- DeepMAsED-SM/bin/scripts/bam2feat.py
+++ DeepMAsED-SM/bin/scripts/bam2feat.py
@@ -15,7 +15,13 @@ from multiprocessing import Pool
 ## 3rd party
 import pysam
 
-# UI
+# logging
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
+                      argparse.RawDescriptionHelpFormatter):
+    pass
+
+# argparse
 desc = 'Creating DL features from bam file'
 epi = """DESCRIPTION:
 The bam file should be indexed via `samtools index`.
@@ -25,7 +31,7 @@ The '--short' list of features is the features used for
 DeepMAsED version1. Using '--short' will generate a 
 feature table that is much smaller (many fewer features).
 
-The output table is written to STDOUT.
+The output tab-delimited feature table is written to STDOUT.
 """
 parser = argparse.ArgumentParser(description=desc,
                                  epilog=epi,
@@ -35,28 +41,36 @@ parser.add_argument('bam_file', metavar='bam_file', type=str,
 parser.add_argument('fasta_file', metavar='fasta_file', type=str,
                     help='Reference sequences for the bam (sam) file')
 parser.add_argument('-a', '--assembler', type=str, default='unknown',
-                    help='Name of metagenome assembler used to create the contigs (default: %(default)s)')
+                    help='Name of metagenome assembler used to create the contigs')
 parser.add_argument('-b', '--batches', type=int, default=100,
-                    help='Number of contigs batches for parallel processing (default: %(default)s)')
+                    help='Number of contigs batches for parallel processing')
 parser.add_argument('-c', '--chunks', type=int, default=50,
-                    help='No. of bins to process before writing; lower values = lower memory (default: %(default)s)')
+                    help='No. of bins to process before writing; lower values = lower memory')
 parser.add_argument('-p', '--procs', type=int, default=1,
                     help='Number of parallel processes (default: %(default)s)')
 parser.add_argument('-w', '--window', type=int, default=4,
-                    help='Sliding window size for sequence entropy & GC content (default: %(default)s)')
+                    help='Sliding window size for sequence entropy & GC content')
 parser.add_argument('-s', '--short', action='store_true', default=False,
-                    help='Short feature list instead of all features? (default: %(default)s)')
+                    help='Short feature list instead of all features?')
 parser.add_argument('--debug', action='store_true', default=False,
-                    help='Debug mode; just for troubleshooting (default: %(default)s)')
+                    help='Debug mode; just for troubleshooting')
 parser.add_argument('--version', action='version', version='0.0.1')
 
 
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
-
-
+# global 
 IDX = {'A':0, 'C':1, 'G':2, 'T':3, 'N':-1}
 
+# functions
 def count_SNPs(coverage_by_base, ref_seq):
+    """
+    Count SNPs across the reference
+    
+    Params:
+      coverage_by_base : pysam.AlignmentFile.count_coverage object
+      ref_seq : py.FastaFile.fetch object
+    Return
+      int
+    """
     SNP_cnt = 0
     for i,x in enumerate(coverage_by_base):
         if i != IDX[ref_seq]:
@@ -64,7 +78,8 @@ def count_SNPs(coverage_by_base, ref_seq):
     return SNP_cnt
 
 def entropy(seq):
-    """ Calculate Shannon entropy of sequence.
+    """
+    Calculate Shannon entropy of sequence.
     """
     cnt = [seq.count(i) for i in 'ACGT']
     d = sum(cnt)
@@ -78,14 +93,16 @@ def entropy(seq):
     return round(ent, 3)
 
 def gc_percent(seq):
-    """ Calculate fraction of GC bases within sequence.
+    """ 
+    Calculate fraction of GC bases within sequence.
     """
     counts = [seq.count(i) for i in 'ACGT']
     gc = float(counts[1] + counts[2])/sum(counts)
     return round(gc, 3)
 
 def window(seq, wsize = 4):
-    """ Sliding window of sequence
+    """
+    Sliding window of sequence
     """
     it = iter(seq)
     win = deque((next(it, None) for _ in range(wsize)), maxlen=wsize)
@@ -96,7 +113,14 @@ def window(seq, wsize = 4):
         yield win
 
 def seq_entropy(seq, window_size):
-    """ Calculate the sequence entropy across a sliding window
+    """
+    Calculate the sequence entropy across a sliding window
+    
+    Params:
+      seq : py.FastaFile.fetch object
+      window_size : int
+    Return:
+      sequence_entropy (float), sequence_G+C (float)
     """
     if window_size > int(len(seq)/2.0):
         window_size = int(len(seq)/2.0)
@@ -122,7 +146,13 @@ def seq_entropy(seq, window_size):
     return ent, gc
     
 def _contig_stats(contig, bam_file, fasta_file, assembler, window_size, short):
-    """ Extracting contig-specific info from bam file
+    """ 
+    Extracting contig-specific info from bam file.
+    
+    Params:
+      See contig_stats()
+    Returns:
+      [contig1_stats, ..., contigN_stats]
     """
     logging.info('  Processing contig: {}'.format(contig))
     
@@ -299,10 +329,20 @@ def _contig_stats(contig, bam_file, fasta_file, assembler, window_size, short):
             stats.append(x)
             
         return stats
-
     
 def contig_stats(contigs, bam_file, fasta_file, assembler, window_size, short):
-    """ Extracting contig-specific info from all contigs
+    """ 
+    Extracting contig-specific info from all contigs
+    
+    Params:
+      contigs : pysam.AlignmentFile.references object
+      bam_file : str; bam file path
+      fasta_file : str; bam file path
+      assembler : str; which assembler used?
+      window_size : int; window size for calculating window-based stats
+      short : bool; just short feature list?
+    Returns:
+      [contig1_stats, ..., contigN_stats]
     """
     stats = []
     for contig in contigs:
@@ -312,9 +352,15 @@ def contig_stats(contigs, bam_file, fasta_file, assembler, window_size, short):
     return stats
 
 def batch_contigs(contigs, n_batches):
-    """ Processing contigs in batches
+    """
+    Processing contigs in batches. 
+    
+    Params: 
+      contigs : pysam.AlignmentFile.references object
+      n_batches : int; number of batch
+
     Returns:
-      dict : {binID : [contig1, ..., contigN]}
+      dict : {bin_id : [contig1, ..., contigN]}
     """
     n_contigs = len(contigs)
     if n_contigs < n_batches:
@@ -334,7 +380,9 @@ def batch_contigs(contigs, n_batches):
     return list(contig_bins.values())
 
 def write_stats(stats):
-    # printing results
+    """
+    Pretty print of the results
+    """
     logging.info('Writing features...')
     for batch in stats:
         for y in batch:
@@ -342,9 +390,10 @@ def write_stats(stats):
                 print('\t'.join(z))
 
 def main(args):
-    """ Main interface
+    """ 
+    Main interface
     """
-    # header
+    # output table header
     H = ['assembler', 'contig',  'position', 'ref_base', 
          'num_query_A', 'num_query_C', 'num_query_G', 'num_query_T',
          'num_SNPs', 'coverage', 'num_discordant']
@@ -393,13 +442,14 @@ def main(args):
     if args.debug:
         contigs = contigs[len(contigs)-10:]
     
-    # getting contig stats
+    # getting contig stats (in parallel)
     func = partial(contig_stats, bam_file=args.bam_file,
                    fasta_file=args.fasta_file,
                    assembler=args.assembler,
                    window_size=args.window,
                    short=args.short)
     if args.debug is False and args.procs > 1:
+        # with parallel processing
         p = Pool(args.procs)
         # batching contigs for multiprocessing
         contig_bins = batch_contigs(contigs, args.batches)
@@ -421,7 +471,7 @@ def main(args):
             x = i
             stats = None
     else:
-        # getting stats
+        # no parallel processing
         stats = map(func, [contigs])
         write_stats(stats)
         
