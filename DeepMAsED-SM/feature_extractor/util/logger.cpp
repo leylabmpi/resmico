f8378e74c7a9896579eed2cbda1ed3f3cad4d269
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2021-07-20 08:55:32 +0200

    rn DeepMAsED-SM

--- DeepMAsED-SM/feature_extractor/util/logger.cpp
+++
@@ -1,8 +0,0 @@
-#include "util/logger.hpp"
-#include <spdlog/sinks/stdout_color_sinks.h>
-
-std::shared_ptr<spdlog::logger> logger() {
-    static const std::shared_ptr<spdlog::logger> LOGGER
-            = spdlog::stderr_color_mt("svc", spdlog::color_mode::always);
-    return LOGGER;
-}
