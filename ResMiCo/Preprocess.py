92b4d3b2c456fc3b10fdd79287cf1ed43e98352e
Olga Mineeva <omineeva@student.ethz.ch>
2021-08-26 13:12:29 +0200

    preprocessing

--- ResMiCo/Preprocess.py
+++ ResMiCo/Preprocess.py
@@ -9,13 +9,12 @@ def main(args):
             Utils.pickle_in_parallel(feature_files_dic, args.n_procs, set_target=False, real_data=True)
         else:
             feature_files_dic = Utils.read_feature_file_table(args.feature_file_table,
-                                                              args.force_overwrite, technology=args.technology,
-                                                              longdir=args.longdir)
+                                                              args.force_overwrite, technology=args.technology)
             Utils.pickle_in_parallel(feature_files_dic, args.n_procs,
                                      args.set_target, real_data=False, 
-                                     v1=args.deepmased_v1, longdir=args.longdir)
+                                     v1=args.deepmased_v1)
     if args.compute_mean_std:
-        Utils.compute_sum_sumsq_n(args.feature_file_table, n_feat=20)  # todo: features_sel
+        Utils.compute_sum_sumsq_n(args.feature_file_table, n_feat=20) 
     if args.standard_data:
         if args.real_data:
             Utils.standardize_data(args.feature_file_table, args.mean_std_file, set_target=False, 
