9b73fad20f34f7b7d04e09c085d790df15e381c7
Daniel Danciu <dd@daedalean.ai>
2021-09-28 16:57:05 +0200

    REmove thread pool

--- ResMiCo/ContigReader.py
+++ ResMiCo/ContigReader.py
@@ -176,7 +176,7 @@ class ContigReader:
         result = []
         # TODO: try using pool.map() instead for larger batch-sizes; for the small test set, not using a pool
         #  is 100x faster (probably bc. mini-batch size is only 6)
-        for contig_data in ContigReader.pool.map(self._read_and_normalize, contig_infos):
+        for contig_data in map(self._read_and_normalize, contig_infos):
             result.append(contig_data)
         logging.info(f'Contigs read in {(timer() - start):5.2f}s; read: {self.read_time:5.2f}s '
                      f'normalize: {self.normalize_time:5.2f}s')
