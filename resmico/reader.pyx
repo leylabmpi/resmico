86f321216875e52fca2ae49869c313df5f3074f2
Daniel Danciu <dd@daedalean.ai>
2021-11-15 09:57:54 +0100

    Normalize entropy

--- resmico/reader.pyx
+++ resmico/reader.pyx
@@ -27,7 +27,8 @@ float_feature_tuples = [('min_insert_size_Match', np.uint16, np.float32),
                         ('min_al_score_Match', np.int8, np.float32),
                         ('mean_al_score_Match', np.float32, np.float32),
                         ('stdev_al_score_Match', np.float32, np.float32),
-                        ('max_al_score_Match', np.int8, np.float32), ]
+                        ('max_al_score_Match', np.int8, np.float32),
+                        ('seq_window_entropy', np.float32, np.float32)]
 feature_tuples = [('ref_base', np.uint8, np.float32),  # because we use one-hot encoding, so 4 bytes
                   ('coverage', np.uint16, np.uint16),
                   ('num_query_A', np.uint16, np.float32),
@@ -41,7 +42,7 @@ feature_tuples = [('ref_base', np.uint8, np.float32),  # because we use one-hot 
                    ('num_orphans_Match', np.uint16, np.float32),
                    ('num_proper_SNP', np.uint16, np.float32),
                    ('seq_window_perc_gc', np.float32, np.float32),
-                   ('seq_window_entropy', np.float32, np.float32)]
+                   ]
 
 float_feature_names = [f[0] for f in float_feature_tuples]
 
