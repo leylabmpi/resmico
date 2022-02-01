3e897369a4a42f74db8b3157371c7bd15835eb02
Daniel Danciu <dd@daedalean.ai>
2022-02-01 15:50:08 +0100

    Fix breakpoint computation. Eliminate residual avg network

--- resmico/models_fl.py
+++ resmico/models_fl.py
@@ -182,19 +182,18 @@ class Resmico(object):
             x = Dropout(rate=self.dropout)(x)
             x = Bidirectional(LSTM(40, return_sequences=False, dropout=0.0), merge_mode="concat")(x)
 
-        elif self.net_type in ['cnn_resnet', 'cnn_resnet_argmax', 'cnn_resnet_avg', 'cnn_resnet_residual_avg']:
+        elif self.net_type in ['cnn_resnet', 'cnn_resnet_argmax', 'cnn_resnet_avg']:
             x = BatchNormalization()(inlayer)
             x = Conv1D(self.filters, kernel_size=10,
                        input_shape=(None, self.n_feat),
                        padding='valid', name='1st_conv')(x)
             x = utils.relu_bn(x)
             num_filters = self.filters
-            residual_block = utils.residual_block if self.net_type == 'cnn_resnet_residual_avg' else utils.residual_block_all_valid
             # for each block group, create the requested number of residual blocks
             for i, num_blocks in enumerate(self._get_blocks(self.num_blocks)):
                 for j in range(num_blocks):
-                    x = residual_block(x, downsample=(j == 0 and i != 0), filters=num_filters,
-                                       kernel_size=self.ker_size)
+                    x = utils.residual_block(x, downsample=(j == 0 and i != 0), filters=num_filters,
+                                             kernel_size=self.ker_size)
                 num_filters *= 2
 
             # lambda function that computes the data size after applying all the convolutional
@@ -253,7 +252,7 @@ class Resmico(object):
             for i, num_blocks in enumerate(self._get_blocks(self.num_blocks)):
                 for j in range(num_blocks):
                     x = utils.old_residual_block(x, downsample=(j == 0 and i != 0), filters=num_filters,
-                                             kernel_size=self.ker_size)
+                                                 kernel_size=self.ker_size)
                 num_filters *= 2
                 # this is needed only to avoid errors, mask is not used later
             tmp_model = Model(inputs=inlayer, outputs=x)  # dummy model used only in next line
